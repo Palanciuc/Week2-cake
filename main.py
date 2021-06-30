173ae65bd48fbec967d8b8316349975aa2d0f965
PalanciucDragos <>
2021-06-30 13:44:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print('Flour')
+
+counter = 0 #Used to control the loop
+
+while(counter < 3):
+ print('1 Egg')
+ counter += 1
+
+print('Milk')
+
+counter = 0
+while(counter < 2):
+  print('1 Sugar')
+  counter += 1
+
+print('Baking Soda')
+
+counter = 0
+while(counter < 60):
+  print('1 minute Bake')
+  counter += 1
+
+counter = 0 
+while(counter < 4):
+  print('Eat a slice of cozonac')
+  counter += 1
